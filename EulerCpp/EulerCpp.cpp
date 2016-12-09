c40cf7019675259ad736ea8358836b983fd03a8e
crea7or <pavel.sokolov@gmail.com>
2016-12-10 01:06:58 +0300

    Seems faster...

--- EulerCpp/EulerCpp.cpp
+++ EulerCpp/EulerCpp.cpp
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <vector>
 #include <unordered_map>
+#include <bitset>
 #include <map>
 #include <chrono>
 
@@ -21,56 +22,99 @@ typedef std::chrono::high_resolution_clock high_resolution_clock;
 typedef std::chrono::milliseconds milliseconds;
 
 // number of ^5 powers to check
-
- const int N = 150;
+const int N = 250; // max number is ~7131 for 64 bit values ( 18 446 744 073 709 551 616 )
 // N=150 27 ^ 5 + 84 ^ 5 + 110 ^ 5 + 133 ^ 5 = 144 ^ 5 (Lander & Parkin, 1966).
-
-// const int N = 86000;
-// N=86000 for 55^5 + 3183^5 + 28969^5 + 85282^5 = 85359^5 (Frye, 2004).
+// const int N = 86000; // BigInteger is required to operate with these values (128 bit at least)
+// N=86000 for 55^5 + 3183^5 + 28969^5 + 85282^5 = 85359^5 (Frye, 2004). ( 85359^5 is 4 531 548 087 264 753 520 490 799 )
 
 typedef unsigned long long ullong;
+typedef unsigned int uint;
 
 ullong counter = 0;
 ullong powers[N];
+std::unordered_map<ullong, uint> all;
+uint bitseta[256 * 256 * 8];
 
 ullong p5(ullong x)
 {
 	return x * x * x * x * x;
 }
 
-std::unordered_map<ullong, unsigned int> all;
+inline bool findBit64(ullong v)
+{
+	uint bitval = (((uint)((v >> 32) ^ v)) & 0x00FFFFFF);
+	uint b = 1 << (bitval & 0x1F);
+	uint index = bitval >> 5;
+	return ((bitseta[index] & b) > 0);
+}
+
+inline void setBit64(ullong v)
+{
+	uint bitval = (((uint)((v >> 32) ^ v)) & 0x00FFFFFF);
+	uint b = 1 << (bitval & 0x1F);
+	uint index = bitval >> 5;
+	bitseta[index] = bitseta[index] | b;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	high_resolution_clock::time_point _start = high_resolution_clock::now();
 
+	ullong hashHit = 0;
+	ullong sum = 0;
+	counter = 0;
+	std::chrono::milliseconds speedTime;
+	double speed, hitsspeed;
+
+	uint ind0 = 0x02;
+	uint ind1 = 0x02;
+	uint ind2 = 0x02;
+	uint ind3 = 0x02;
 
 	cout << "Building table of powers 1 - " << N << "^5\n";
-	for (unsigned int i = 0; i < N; ++i)
+
+	all.reserve( N +1 );
+	memset(bitseta, 0, sizeof(bitseta));
+
+	for (uint i = 0; i < N; ++i)
 	{
 		powers[i] = p5(i);
 		all[powers[i]] = i;
+		setBit64(powers[i]);
 	}
 
-	cout << "Table ready, starting search...\n\n";
-
-	ullong sum = 0;
-
-	unsigned int ind0 = 0x02;
-	unsigned int ind1 = 0x02;
-	unsigned int ind2 = 0x02;
-	unsigned int ind3 = 0x02;
-
-	counter = 0;
-	std::chrono::milliseconds speedTime;
-	double speed;
+	speedTime = std::chrono::duration_cast<milliseconds>(high_resolution_clock::now() - _start);
+	cout << "Table ready. Building time: " << ((double)speedTime.count() / 1000 ) << "s, starting search...\n\n";
 
 	while (true)
 	{
 		sum = powers[ind0] + powers[ind1] + powers[ind2] + powers[ind3];
+		if (findBit64(sum))
+		{
+			hashHit++;
+			if (all.find(sum) != all.end())
+			{
+				for (int i = 0; i < 78; ++i)
+				{
+					cout << " ";
+				}
+				cout << "\r";
+				cout << "\n\nOk, found: ";
+				cout << ind3 << "^5 + ";
+				cout << ind2 << "^5 + ";
+				cout << ind1 << "^5 + ";
+				cout << ind0 << "^5 = ";
+				cout << all[sum] << "^5  ";
 
+				speedTime = std::chrono::duration_cast<milliseconds>(high_resolution_clock::now() - _start);
+				cout << "s: " << (double(speedTime.count()) / 1000) << " s. itr: " << counter << "\n\n";
+				//break;
+			}
+		}
+
+		// displaying some progress
 		counter++;
-		if ((counter & 0xFFFFFF ) == 0 )
+		if ((counter & 0xFFFFFF) == 0)
 		{
 			for (int i = 0; i < 78; ++i)
 			{
@@ -84,24 +128,11 @@ int _tmain(int argc, _TCHAR* argv[])
 
 			speedTime = std::chrono::duration_cast<milliseconds>(high_resolution_clock::now() - _start);
 			speed = ((double)counter / (double)speedTime.count());
-			cout << " speed: " << speed << " itr/ms  total itr: " << counter << "   \r";
+			hitsspeed = ((double)counter / (double)hashHit);
+			// speed: iterations per millisecond, counter: total iterations, hitspeed: hash filter performance
+			cout << "s: " << speed << " i/ms itrs: " << counter << " hh: " << hitsspeed << "\r";
 		}
-
-		if (all.find(sum) != all.end())
-		{
-			for (int i = 0; i < 78; ++i)
-			{
-				cout << " ";
-			}
-			cout << "\r";
-			cout << "\n\nOk, found: ";
-			cout << ind3 << "^5 + ";
-			cout << ind2 << "^5 + ";
-			cout << ind1 << "^5 + ";
-			cout << ind0 << "^5 = ";
-			cout << all[sum] << "^5\n\n";
-			//break;
-		}
+		// displaying some progress
 
 		++ind0;
 		if (ind0 < N)
@@ -133,11 +164,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 
 	std::chrono::milliseconds time = std::chrono::duration_cast<milliseconds>(high_resolution_clock::now() - _start);
-	//speed = double(time.count()) / 1000;
-
 	cout << "\nDone in: " << ( double(time.count()) / 1000 ) << "s\n" << "Total iterations: " << counter << "\n";
 	speed = ((double)counter / (double)time.count());
 	cout << "Speed: " << speed << " itr/ms";
+	hitsspeed = ((double)counter / (double)hashHit);
+	cout << "\nHash filter ratio: " << hitsspeed << "\n\nPress Enter...";
 
 	getchar();
 
